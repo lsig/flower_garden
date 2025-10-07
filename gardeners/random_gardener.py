ff769c0c1658289a5e9dfdcf3f1383f9d3e6b4a0
lsig <logis21@ru.is>
2025-10-06 23:43:36 -0400

    [Simulator] add a random player strategy

--- gardeners/random_gardener.py
+++ gardeners/random_gardener.py
@@ -1,6 +1,9 @@
+import random
+
 from core.garden import Garden
 from core.gardener import Gardener
 from core.plants.plant_variety import PlantVariety
+from core.point import Position
 
 
 class RandomGardener(Gardener):
@@ -8,4 +11,10 @@ class RandomGardener(Gardener):
         super().__init__(garden, varieties)
 
     def cultivate_garden(self) -> None:
-        pass
+        for variety in self.varieties:
+            x = random.uniform(0, self.garden.width)
+            y = random.uniform(0, self.garden.height)
+
+            position = Position(x, y)
+
+            self.garden.add_plant(variety, position)
