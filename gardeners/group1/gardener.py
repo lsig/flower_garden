5aabd042de2f0ae84719f0f283802a7788c2b6aa
soumilbaldota <ssb2234@columbia.edu>
2025-10-22 12:25:06 -0400

    fix: organize imports

--- gardeners/group1/gardener.py
+++ gardeners/group1/gardener.py
@@ -2,6 +2,7 @@ from core.garden import Garden
 from core.gardener import Gardener
 from core.plants.plant_variety import PlantVariety
 
+
 class Gardener1(Gardener):
     def __init__(self, garden: Garden, varieties: list[PlantVariety]):
         super().__init__(garden, varieties)
